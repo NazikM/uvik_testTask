cdef4c06f68ef22bb8d52d3ae0ee236ebf8ce56a
Nazar Marusyn <marusinnazar@gmail.com>
2022-10-08 13:44:33 +0300

    Final improvement for solution.py

--- task_1/solution.py
+++ task_1/solution.py
@@ -1,17 +1,20 @@
 import csv
 
-res_dict = {}
 
-with open('data.csv', encoding='utf-8') as f:
-    csv_reader = csv.reader(f, delimiter=',')
-    next(csv_reader)
-    for row in csv_reader:
-        country = res_dict.get(row[0], {'people': [], 'count': 0})
-        res_dict.update({
-            row[0]: {
-                'people': country['people'] + [row[1]],
-                'count': country['count'] + 1
-            }
-        })
+def get_res(filename='data.csv'):
+    res_dict = {}
+    with open(filename, encoding='utf-8') as f:
+        csv_reader = csv.reader(f, delimiter=',')
+        next(csv_reader)
+        for row in csv_reader:
+            country = res_dict.get(row[0], {'people': [], 'count': 0})
+            res_dict.update({
+                row[0]: {
+                    'people': country['people'] + [row[1]],
+                    'count': country['count'] + 1
+                }
+            })
+    return res_dict
 
-print(res_dict)
+
+print(get_res())
