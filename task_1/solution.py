9482383abdc1c4cd372aea81fc86311876ea12ae
Nazar Marusyn <marusinnazar@gmail.com>
2022-10-08 13:17:41 +0300

    Little performance improvement.

--- task_1/solution.py
+++ task_1/solution.py
@@ -6,10 +6,11 @@ with open('data.csv', encoding='utf-8') as f:
     csv_reader = csv.reader(f, delimiter=',')
     next(csv_reader)
     for row in csv_reader:
-        country = res_dict.update({
+        country = res_dict.get(row[0], {'people': [], 'count': 0})
+        res_dict.update({
             row[0]: {
-                'people': res_dict.get(row[0], {'people': []})['people'] + [row[1]],
-                'count': res_dict.get(row[0], {'count': 0})['count'] + 1
+                'people': country['people'] + [row[1]],
+                'count': country['count'] + 1
             }
         })
 
