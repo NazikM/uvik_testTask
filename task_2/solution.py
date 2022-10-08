b863a36346c0333a09f1894598bbc5aa1440521d
Nazar Marusyn <marusinnazar@gmail.com>
2022-10-08 15:48:06 +0300

    Add methods __init__ and get_tasks for Model

--- task_2/solution.py
+++ task_2/solution.py
@@ -1,5 +1,18 @@
+import json
+
+
 class Model:
-    ...
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.activeTasks = []
+        self.completedTasks = {}
+
+    def get_tasks(self):
+        with open(self.file_name, encoding="utf-8") as file:
+            data = json.load(file)
+        self.activeTasks = data["activeTasks"]
+        self.completedTasks = data["completedTasks"]
+
 
 
 class View:
