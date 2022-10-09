d8910c1685db4c38bd256be0f6be90038f2ec329
Nazar Marusyn <marusinnazar@gmail.com>
2022-10-09 15:10:30 +0300

    Add test to storage.json and update solution logic

--- task_2/solution.py
+++ task_2/solution.py
@@ -2,49 +2,72 @@ import json
 import os
 from datetime import date
 from typing import List, Dict
+from colorama import Fore
+from colorama import Style
 
 
 class Model:
     def __init__(self, file_name):
         self.file_name = file_name
-        self.activeTasks = []
-        self.completedTasks = {}
+        self._tasks = []
+        self._lastId = None
 
     def load_tasks(self) -> None:
         with open(self.file_name, encoding="utf-8") as file:
             data = json.load(file)
-        self.activeTasks = data["activeTasks"]
-        self.completedTasks = data["completedTasks"]
+        self._tasks = data["tasks"]
+        self._lastId = data["lastId"]
 
-    def get_activeTasks(self) -> List:
-        return self.activeTasks
+    def get_tasks(self):
+        return self._tasks
 
-    def get_completedTasks(self) -> Dict:
-        return self.completedTasks
+    # def get_activeTasks(self) -> List:
+    #     for task in self.tasks:
+    #         if not task["completed"]:
+    #             yield task
+
+    # def get_completedTasks(self) -> Dict:
+    #     for task in self.tasks:
+    #         if task["completed"]:
+    #             yield task
 
     def add_tasks(self, tasks: List[str]) -> List[str]:
         for task_name in tasks:
-            self.activeTasks.append(task_name)
-        return self.activeTasks
+            self._lastId += 1
+            self._tasks.append({
+                "id": self._lastId,
+                "task": task_name,
+                "completed": False
+            })
+        return self._tasks
 
-    def remove_tasks(self, tasks: List[int]) -> List[str]:
-        return [self.activeTasks.pop(task_id) for task_id in tasks]
+    def remove_tasks(self, tasks: List[str]) -> List[str]:
+        response = []
+        for task_id in tasks:
+            for i in range(len(self._tasks)):
+                if self._tasks[i]['id'] == int(task_id):
+                    response.append(self._tasks.pop(i))
+                    break
+            else:
+                response.append(f"There is no task with id {task_id}")
+        return response
 
-    def mark_as_done(self, task_id: int) -> str:
-        tasks = self.remove_tasks(tasks=[task_id])
-
-        today_date = date.today().strftime("%d/%m/%Y")
-
-        self.completedTasks.update({
-            today_date: self.completedTasks.get(today_date, []) + tasks
-        })
-        return tasks[0]
+    def mark_as_done(self, task_id: int) -> Dict:
+        for task in self._tasks:
+            if task['id'] == task_id:
+                task.update({
+                    'completed': True,
+                    'date': date.today().strftime("%d/%m/%Y")
+                })
+                break
+        else:
+            return {}
+        return task
 
     def save(self) -> None:
         with open(self.file_name, 'w', encoding='utf-8') as file:
             json.dump({
-                "activeTasks": self.activeTasks,
-                "completedTasks": self.completedTasks
+                'tasks': self._tasks
             }, file)
 
 
@@ -53,37 +76,48 @@ class View:
     def clean_screen(cls):
         os.system('clear')
 
-    def print_tasks(self, active: List[str], completed: Dict[str, List[str]]):
+    def print_tasks(self, tasks: List[Dict]):
         print("YOUR TODO LIST")
-        print("To be done:")
-        for tk, task in enumerate(active):
-            print(f"{tk}| {task}")
-        print("Completed:")
-        for _, tasks in completed:
-            for tk, task in enumerate(active):
-                print(f"{tk}| {task}")
-            print(f"{tk}| {task}")
+        if tasks:
+            for task in tasks:
+                print(f"{task['id']}| {task['task']}{(Fore.GREEN+' Done'+Style.RESET_ALL)if task['completed'] else ''}")
+        else:
+            print("There are no tasks!")
 
     def menu_view(self):
         msg = """
-            Hello this is TODO app with a command line interface.
+        Hello this is TODO app with a command line interface.
             
-            To start working with that you should tell me number of action.
-            1) Add an item
-            2) Remove an item
-            3) Mark an item as done
-            4) List items
+        To start working with that you should tell me number of action.
+        1) Add an item
+        2) Remove an item
+        3) Mark an item as done
+        4) List items
+        5) Show stat
         """
         print(msg)
 
     def add_tasks_view(self):
         msg = """
-        To add new tasks just type them and press enter. 
+        To add new tasks just type their ids.
+        Every new line == new task. 
+        To exit typing mode just leave empty line.
+        """
+        print(msg)
+    
+    def remove_tasks_view(self):
+        msg = """
+        To remove tasks just type their ids. 
         Every new line == new task. 
         To exit typing mode just leave empty line.
         """
         print(msg)
 
+    def mark_as_done_view(self):
+        msg = """
+        Type 1 id of item that should be marked as done.
+        """
+        print(msg)
 
 class Controller:
     @staticmethod
@@ -108,12 +142,19 @@ class Controller:
                     data = Controller.multiline_input()
                     model.add_tasks(data)
                 case "2":
-                    ...
+                    view.remove_tasks_view()
+                    data = Controller.multiline_input()
+                    res = model.remove_tasks(data)
+                    print(res)
                 case "3":
-                    ...
+                    view.mark_as_done_view()
+                    task_id_input = int(input("::"))
+                    res = model.mark_as_done(task_id_input)
+                    print(res)
                 case "4":
-                    view.print_tasks(active=model.get_activeTasks(),
-                                     completed=model.get_completedTasks())
+                    view.print_tasks(tasks=model.get_tasks())
+                case "5":
+                    ...
 
 
 if __name__ == "__main__":
